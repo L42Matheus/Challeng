f19432a01dc46942d1c0873079bb992d7dc236d9
matheus <lmatheus.tome@gmail.com>
2023-12-08 18:40:57 -0300

    Refactor 'run()' method and introduce 'getAvailableCustomerSuccess()'

--- src/main/java/com/rdstation/CustomerSuccessBalancing.java
+++ src/main/java/com/rdstation/CustomerSuccessBalancing.java
@@ -5,19 +5,26 @@ import java.util.*;
 public class CustomerSuccessBalancing {
 
     private final List<CustomerSuccess> customerSuccess;
-    private final List<Customer> customers;
+    private final List<Customer> customerList;
     private final List<Integer> customerSuccessAway;
 
     public CustomerSuccessBalancing(List<CustomerSuccess> customerSuccess,
-                                    List<Customer> customers,
+                                    List<Customer> customerList,
                                     List<Integer> customerSuccessAway) {
         this.customerSuccess = customerSuccess;
-        this.customers = customers;
+        this.customerList = customerList;
         this.customerSuccessAway = customerSuccessAway;
     }
 
 
     public int run() {
+        List<CustomerSuccess> availablesCustomerSuccess = getAvailableCustomerSuccess();
+
+
         return 0;
+    }
+
+    private List<CustomerSuccess> getAvailableCustomerSuccess() {
+        return null;
     }
 }
